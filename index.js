ce32294e534376504305b43070d03e72a16db37c
tomoyahiroe <tomoya4creative@gmail.com>
2022-08-10 03:29:48 +0900

    add: octokit package

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-import {graphql} from '@octokit/graphql';
+import { graphql } from "@octokit/graphql";
+import * as readline from "readline";
 const token = process.env.GIT_EXTENSION_TOKEN;
 
 const graphqlWithAuth = graphql.defaults({
- headers: {
-  authorization: `token ${token}`,
- },
+  headers: {
+    authorization: `token ${token}`,
+  },
 });
-import * as readline from 'readline';
+
 const QUERY = `
 	query getUser($login: String!) {
 		user(login: $login) {
@@ -23,20 +24,19 @@ const QUERY = `
 `;
 
 async function getInfo(username) {
-	try {
-		const {user} = await graphqlWithAuth(QUERY, {login: username});
-		console.log(user);
-	} catch(err) {
-		console.error(err.message);
-	}
+  try {
+    const { user } = await graphqlWithAuth(QUERY, { login: username });
+    console.log(user);
+  } catch (err) {
+    console.error(err);
+  }
 }
 
-const reader=readline.createInterface({
-	input: process.stdin,
-	output: process.stdout
-})
-reader.question("INPUT TARGET USERNAME\n",
-inS=>{
-	reader.close();
-	getInfo(inS)
-})
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+reader.question("INPUT TARGET USERNAME\n", (inS) => {
+  reader.close();
+  getInfo(inS);
+});
